3e63915d3e31969aba69fc9abe86b82b48fc5abe
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2024-09-11 19:34:17 +0200

    added meetum and admin class

--- meetup_site/meet_app/admin.py
+++ meetup_site/meet_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from . models import Meetup
 # Register your models here.
+
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_filter = ['date']
+    prepopulated_fields = {'slug':('title',)}
+    
+
+admin.site.register(Meetup, MeetupAdmin)
+
--- meetup_site/meet_app/models.py
+++ meetup_site/meet_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    address = models.CharField(max_length=100)
+    date = models.DateField() 
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
